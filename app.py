1b96dd4c8a93a2193461e0d674c7afb9e82e292f
satyam <satyamtamrakar2002@tutamail.com>
2024-11-06 15:55:26 +0530

    fixes workflow

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
 from flask import Flask
-
+import os
+load_dotenv()
 app=Flask(__name__)
 
+APP_NAME=os.getenv('APP_NAME','Alien APP')
 @app.route('/')
 def hello_world():
-    return {"message":'Hello, World!(From Flask App) v2'}
+    return {"message":f'Hello, World!(From Flask App) v2 ,APP_NAME:{APP_NAME}'}
 
 app.run(host='0.0.0.0',port=8080)
