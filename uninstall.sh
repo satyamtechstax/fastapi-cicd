f1f5fb6b88cd6e6f9515785f3d85f75d3387ee6b
satyam <satyamtamrakar2002@tutamail.com>
2024-11-06 15:03:34 +0530

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
 echo "Stoping and disabling fastAPI service..."
 systemctl stop fastapi.service
-systemctl disabled fastapi.service
+systemctl disable fastapi.service
 echo "Uninstalled Fast API service"
